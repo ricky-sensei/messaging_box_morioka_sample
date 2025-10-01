284e84cbde8814c9b90f785252509d58bf9cd7a3
ricky-sensei <axcelcamp.teacher@gmail.com>
2025-10-01 23:27:54 +0900

    04_login

--- app.py
+++ app.py
@@ -1,48 +1,78 @@
-# postリクエストを受け取れるようにrequestをついか
-# 不正な値を入力した場合にリダイレクトするためにredirectを追加
-# フラッシュメッセージを出すためにflash,  を追加
 from flask import Flask, flash, redirect, render_template, request  
-from werkzeug.security import generate_password_hash  # パスワードのハッシュ化
+from flask_login import LoginManager, login_user # ログイン関連
+from werkzeug.security import generate_password_hash, check_password_hash  # 入力されたｐｗとＤＢ上のｐｗを比較
 from config import User
 
 app = Flask(__name__)
-# flashメッセージ表示のためのシークレットキー
 app.secret_key = "secret"
 
+# LoginManagerの設定
+login_manager = LoginManager()  # LoginManagerのインスタンス化
+login_manager.init_app(app)     # appのログイン情報を初期化
+
+# ユーザー情報を取得するためのメソッド
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_by_id(user_id)
 
 
-# ユーザー登録フォームのルーティングを作成
-@app.route("/register", methods=["GET", "POST"])  # postリクエストを受け取れるようにする
+
+
+
+@app.route("/register", methods=["GET", "POST"])  
 def register():
-    # リクエストメソッドがPOSTだったとき->index.html
+    
     if request.method == "POST":
-        # 空のデータが入力された場合
+        
         if not request.form["name"] or request.form["password"] or request.form["email"]:
-            # フラッシュメッセージをregisterテンプレートに送る
             flash("未入力の項目があります。")
             return redirect(request.url)
         
-        # データの重複があった場合(name, email)
-        if User.select().where(User.name == request.form["name"]):  # dbに同じ名前が存在したら
-            # フラッシュメッセージをregisterテンプレートに送る
+        
+        if User.select().where(User.name == request.form["name"]):  
+            
             flash("その名前はすでに使われています。")
             return redirect(request.url)
 
-        if User.select().where(User.email == request.form["email"]):  # dbに同じemailが存在したら
-            # フラッシュメッセージをregisterテンプレートに送る
+        if User.select().where(User.email == request.form["email"]):  
+            
             flash("そのメールアドレスはすでに使われています。")
             return redirect(request.url)
 
-        # ユーザーをデータベースに追加:Create
+        
         User.create(
             name=request.form["name"],
             email=request.form["email"],
-            password=generate_password_hash(request.form["password"]),  # パスワードのハッシュ化
+            password=generate_password_hash(request.form["password"])
         )
         return render_template("index.html")
 
-    # リクエストメソッドがGETだったとき->register.html
+    
     return render_template("register.html")
+
+
+# login用のルーティングを作成
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    # postリクエストのときに
+    if request.method == "POST":
+        # 未入力処理
+        if not request.form["password"] or not request.form["email"]:
+            flash("未入力の項目があります")
+            return redirect(request.url)
+        
+        # ユーザー認証
+        user = User.select().where(User.email == request.form["email"]).first()  # メールアドレスが合致するユーザーの最初のレコード
+        if user is not None and check_password_hash(user.password, request.form["password"]):  # 合致するレコードが存在していて、パスワードがＤＢのものと合致した場合
+            login_user(user)  # ログイン処理
+            flash(f"ようこそ！{user.name}さん")  # フラッシュメッセージ
+            return redirect("/")  # index.htmlを表示
+
+        # 認証失敗のときのフラッシュメッセージ
+        flash("認証に失敗しました")
+        
+        
+    return render_template('login.html')
 
 
 @app.route("/")
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
+# flaskでログイン機能を実装するためのクラスを追加 モデルに継承させることでログイン状態の管理などができる
+# モデル＝テーブルを操作するためのクラス
+from flask_login import UserMixin
 from peewee import SqliteDatabase, Model, IntegerField, CharField, TextField
 
 db = SqliteDatabase("db.sqlite")
 
 
-class User(Model):
+# UserMixinクラスを継承
+class User(Model, UserMixin):
     id = IntegerField(primary_key=True)
     name = CharField(unique=True)
     email = CharField(unique=True)
